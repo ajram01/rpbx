3360c20df1aef6492a8dc858f43775a8286fe752
Salvador Pruneda <spruneda134@gmail.com>
2025-07-11 13:55:15 -0500

    added to the home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,10 @@ export default function Home() {
         <div className="flex flex-row">
           <div className="flex-1 flex justify-end items-center p-[15px]">
             <div className="flex flex-col items-center w-[560px]">
-              <h1>RPBX</h1>
-              <p>form will go in here somewhere</p>
+              <h1>Unlock Your Business Potential</h1>
+              <button>Sign In</button>
+              <p>By clicking Continue to join or sign in, you agree to RioPlex’s User Agreement, Privacy Policy, and Cookie Policy.</p>
+              <p>New to RioPlex? Join Now</p>
             </div>
           </div>
           <div className="flex-1">
@@ -37,45 +39,86 @@ export default function Home() {
       </div>
 
       {/* Div 3: 3 rows */}
-      <div className="flex flex-col items-center bg-[url('/images/backgrounds/white-bg.png')] bg-cover bg-center p-[15px]">
+      <div className="flex flex-col items-center bg-[url('/images/backgrounds/white-bg.png')] bg-cover bg-center py-[15px]">
 
         <div className="flex flex-row gap-x-[15px] ">
           <div className="flex-1 flex justify-end">
-            <div className="bg-green-200 flex flex-col items-start w-[560px]">
-              <h2>RPBX</h2>
-              <p>text or something</p>
+            <div className="flex flex-col items-start w-[560px]">
+              <h2>Explore our Blog topics</h2>
+              <p>RPBX is here to offer you valuable knowledge. We will help guide you in making your next steps.</p>
             </div>
           </div>
           <div className="flex-1 flex justify-start">
-            <div className="bg-green-200 flex flex-col items-center w-[560px]">
-              <h2>RPBX</h2>
-              <p>text or something</p>
+            <div className="flex flex-col items-center w-[560px]">
+              <button>Selling a Business</button>
+              <button>Buying a Business</button>
             </div>
           </div>
         </div>
-        
-        <div className="bg-green-500">Div 3 - Row 2</div>
 
-        <div className="bg-green-600 w-[1140px] flex flex-col items-center">
-          <h2>Here is a heading</h2>
-          <p>Here is some text</p>
+        
+        <div className="flex flex-row gap-x-[15px] w-full">
+          <div className="bg-white shadow-md flex-1 flex justify-end">
+            <div className="flex flex-col items-start w-[560px]">
+              <h2>Who is RPBX for?</h2>
+              <p>Connecting small business owners with the right investors to help them grow, succeed, and achieve their goals. Join RioPlex Business Exchange and be part of a platform built for ambitious businesses and forward-thinking investors.</p>
+              <button>Looking for an investor</button>
+              <button>Looking to invest</button>
+            </div>
+          </div>
+          <div className="flex-1 flex justify-start">
+            <div className=" flex flex-col items-center text-center w-[560px]">
+              <h2>2.3M Bussineses at stake</h2>
+              <p>In the United States, baby boomers own approximately 40% of small businesses, translating to around 2.3 million businesses. As this generation approaches retirement, many companies will be sold or closed. This transition could impact millions of jobs and represent a significant shift in business ownership over the next decade</p>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="w-[1140px] flex flex-col items-center">
+          <h2>Business Solutions</h2>
+          <p>Connect with Our Trusted Advisors for Tailored Business Solutions</p>
           <div className="flex flex-row gap-x-[5px] w-full">
 
-            <div className="bg-green-700 flex-1 flex flex-col items-center">
-              <h4>icon</h4>
-              <p>text possibly</p>
+            <div className="flex-1 flex flex-col items-center">
+              <Image
+                src="/images/icons/solution-icon-1.png"
+                alt="solution-icon-1"
+                width={200}
+                height={200}
+                className="w-full h-auto"
+              />
+              <p>Business Evaluation</p>
             </div>
-            <div className="bg-green-700 flex-1 flex flex-col items-center">
-              <h4>icon</h4>
-              <p>text possibly</p>
+            <div className="flex-1 flex flex-col items-center">
+              <Image
+                src="/images/icons/solution-icon-2.png"
+                alt="solution-icon-2"
+                width={200}
+                height={200}
+                className="w-full h-auto"
+              />
+              <p>Business Evaluation</p>
             </div>
-            <div className="bg-green-700 flex-1 flex flex-col items-center">
-              <h4>icon</h4>
-              <p>text possibly</p>
+            <div className="flex-1 flex flex-col items-center">
+              <Image
+                src="/images/icons/solution-icon-3.png"
+                alt="solution-icon-3"
+                width={200}
+                height={200}
+                className="w-full h-auto"
+              />
+              <p>Business Evaluation</p>
             </div>
-            <div className="bg-green-700 flex-1 flex flex-col items-center">
-              <h4>icon</h4>
-              <p>text possibly</p>
+            <div className="flex-1 flex flex-col items-center">
+              <Image
+                src="/images/icons/solution-icon-4.png"
+                alt="solution-icon-4"
+                width={200}
+                height={200}
+                className="w-full h-auto"
+              />
+              <p>Business Evaluation</p>
             </div>
 
           </div>
@@ -87,8 +130,11 @@ export default function Home() {
       {/* Div 4: 1 div */}
       <div className="bg-purple-300 flex flex-col items-center bg-[url('/images/backgrounds/black-mint-bg.png')] bg-cover bg-center bg-fixed p-[15px]">
         <div className="bg-white flex flex-col items-center items-center w-[1140px] min-h-[300px]">
-          <h2>the newsletter</h2>
-          <p>big chunk of text</p>
+          <h2>Unlock Your Growth with Expert Insights</h2>
+          <p>Join our monthly RPBX newsletter for exclusive resources, investor opportunities, and expert advice to fuel your business success. It’s free, insightful, and spam-free!</p>
+          <p>Join our monthly RPBX newsletter for exclusive resources, investor opportunities, and expert advice to fuel your business success. It’s free, insightful, and spam-free!</p>
+          <button>Join Now</button>
+          <p>By submitting this form, you are consenting to receive marketing emails from: info@rioplexbizx.com. You can revoke your consent to receive emails at any time by using the SafeUnsubscribe® link, found at the bottom of every email. Emails are serviced by Constant Contact</p>
         </div>
       </div>
     </div>
