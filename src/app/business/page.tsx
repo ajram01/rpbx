3b9c75e7b0f1ac4d16e11d77a9116fd9e4b90666
Salvador Pruneda <spruneda134@gmail.com>
2025-09-22 15:24:55 -0500

    added the authform component to investor and business page

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Button from "../components/Button";
+import AuthForm from "../../components/AuthForm";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -26,19 +27,7 @@ export default async function Business() {
             <div className="flex flex-col items-center w-full lg:w-[560px] max-w-lg">
               <h1 className="text-center">Sell A Business</h1>
 
-              <input
-                type="email"
-                placeholder="Email"
-                className="mt-5 w-full px-6 py-2 rounded-full font-medium bg-white"
-              />
-              <button className="mt-5 w-full px-6 py-2 rounded-full font-medium transition bg-white hover:bg-[var(--color-primary)] text-black">
-                Sign In
-              </button>
-
-              <p className="mt-5 text-center">
-                By clicking Continue to join or sign in, you agree to RioPlex’s User Agreement, Privacy Policy, and Cookie Policy.
-              </p>
-              <p className="mt-5 text-center">New to RioPlex? Join Now</p>
+              <AuthForm />
             </div>
           </div>
 
