40eeffe77c6596fcb2123c822d1a3f71571c6818
AJ <aj@Mac.lan>
2025-09-23 10:58:09 -0500

    Resolved Conflicts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,10 @@
 // app/blog/page.tsx
+// app/blog/page.tsx
 import Navbar from "../components/Navbar";
 import Navbar2 from "../components/Navbar-2";
 import NewsletterSignup from "../../components/ui/newsletter";
 import BlogList from "../components/BlogList";
 import type { Metadata } from "next";
-import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Blog | RioPlex Business Exchange",
@@ -12,26 +12,25 @@ export const metadata: Metadata = {
 };
 
 <<<<<<< HEAD
-export default function Blogs() {
-  const isLoggedIn = false;
-=======
 export default async function Blogs() {
 const posts = await blogClient.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
     const isLoggedIn = true;    
->>>>>>> 63c938d (Created the first step of an investor listing themselves on the platform, I also started work oin second step, the preference step)
+=======
+export default function Blogs() {
+  const isLoggedIn = false;
+>>>>>>> b42fadd (added categories to blogs)
 
   return (
     <div>
       <div className="flex flex-col bg-[url('/images/backgrounds/white-bg.png')] bg-repeat bg-top">
+        <div>{isLoggedIn ? <Navbar /> : <Navbar2 />}</div>
         <div>{isLoggedIn ? <Navbar /> : <Navbar2 />}</div>
 
         <div className="flex flex-col w-full lg:w-[1140px] mx-auto py-10 gap-10 px-5 lg:px-0">
           <h1 className="text-center">Blog</h1>
 
           {/* filter/sort/fetch */}
-          <Suspense fallback={<div>Loading blogs...</div>}>
-            <BlogList />
-          </Suspense>
+          <BlogList />
         </div>
       </div>
 
