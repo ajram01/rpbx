3d31b05100ab3d8401fe6915a3cac259316ec3e4
Anthony Ramirez <anthonyj0127@gmail.com>
2025-10-01 15:26:17 -0500

    Fixed the blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,10 @@
 // app/blog/page.tsx
-// app/blog/page.tsx
 import Navbar from "../components/Navbar";
 import Navbar2 from "../components/Navbar-2";
 import NewsletterSignup from "../../components/ui/newsletter";
 import BlogList from "../components/BlogList";
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Blog | RioPlex Business Exchange",
@@ -18,13 +18,14 @@ export default function Blogs() {
     <div>
       <div className="flex flex-col bg-[url('/images/backgrounds/white-bg.png')] bg-repeat bg-top">
         <div>{isLoggedIn ? <Navbar /> : <Navbar2 />}</div>
-        <div>{isLoggedIn ? <Navbar /> : <Navbar2 />}</div>
 
         <div className="flex flex-col w-full lg:w-[1140px] mx-auto py-10 gap-10 px-5 lg:px-0">
           <h1 className="text-center">Blog</h1>
 
           {/* filter/sort/fetch */}
-          <BlogList />
+          <Suspense fallback={<div>Loading blogs...</div>}>
+            <BlogList />
+          </Suspense>
         </div>
       </div>
 
