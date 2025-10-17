6e3cf3389fe39654efb79db6a8c19929508d1bbc
Anthony Ramirez <anthonyj0127@gmail.com>
2025-10-17 11:54:44 -0500

    removed unused link import

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import NavGate from "../components/NavGate";
 import Button from "../components/Button";
 import AuthForm from "../../components/AuthForm";
