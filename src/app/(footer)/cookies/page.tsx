3ed3aa9f3964e1ac537f788b59ce2087f306d733
Salvador Pruneda <spruneda134@gmail.com>
2025-10-27 14:21:55 -0500

    organized files

--- src/app/(footer)/cookies/page.tsx
+++ src/app/(footer)/cookies/page.tsx
@@ -1,6 +1,6 @@
-import NavGate from '../components/NavGate';
+import NavGate from '../../components/NavGate';
 import Link from 'next/link';
-import Accordion from '../../components/ui/accordion';
+import Accordion from '../../../components/ui/accordion';
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
